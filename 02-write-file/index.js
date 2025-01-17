a9df6f4ab41f48be7b62984989c7afd4372fd351
ksenofonus <danilova.ka88@yandex.ru>
2025-01-17 12:02:36 +0400

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const outputStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+rl.setPrompt('Please enter your line ');
+rl.prompt();
+rl.on('line', (data) => {
+  outputStream.write(`${data}`);
+  rl.close();
+});
